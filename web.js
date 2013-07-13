224de018a0ecaf310daee93c37f290eb05f16ce5
Olga Valls <ovalls@gmail.com>
2013-07-13 13:36:37 +0200

    Read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,3 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
 var fs = require("fs");
 var data = fs.readFileSync("index.html", "utf8");
 console.log(data);
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
