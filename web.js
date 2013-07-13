7585dd8d6f1c5b3d9487ad4148f117bfde748b17
Olga Valls <ovalls@gmail.com>
2013-07-13 14:45:20 +0200

    test hello 2

--- web.js
+++ web.js
@@ -1,4 +1,12 @@
-var fs = require("fs");
-var data = fs.readFileSync("index.html", "utf8");
+var express = require('express');
 
-console.log(data);
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World2!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
