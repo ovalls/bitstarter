00ec24ffe0c1de5ed7dc420a6eec6f8ea53d4b94
Olga Valls <ovalls@gmail.com>
2013-07-13 14:47:28 +0200

    test index

--- web.js
+++ web.js
@@ -1,12 +1,4 @@
-var express = require('express');
+var fs = require("fs");
+var data = fs.readFileSync("index.html", "utf8");
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+console.log(data);
